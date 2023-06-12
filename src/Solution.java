f6eb82472e359717dc51276e9310d0f05745e3c2
Heng Rui Kang <ruikang2002@hotmail.com>
2023-06-12 19:59:08 +0800

    Solution for 14. Longest Common Prefix

--- src/Solution.java
+++ src/Solution.java
@@ -96,6 +96,22 @@ public class Solution {
         return str.equals(String.valueOf(reverse));
     }
 
+    //14. Longest Common Prefix
+    public String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
+
+        for (int i = 0; i < strs[0].length() ; i++){
+            char c = strs[0].charAt(i);
+            for (int j = 1; j < strs.length; j ++) {
+                if ( i == strs[j].length() || strs[j].charAt(i) != c)
+                    return strs[0].substring(0, i);
+            }
+        }
+        return strs[0];
+    }
+
     //20. Valid Parentheses
     public boolean isValid(String s) {
         Stack<Character> stack = new Stack<Character>();
