dcb5923113fd59d7c87a1f3cf462caebf43a5700
Heng Rui Kang <125897596+Ruitherli@users.noreply.github.com>
2023-07-12 17:44:18 +0800

    Solution for 150. Evaluate Reverse Polish Notation

--- src/Solution.java
+++ src/Solution.java
@@ -465,6 +465,37 @@ public class Solution {
         }
         return length;
     }
+    //150. Evaluate Reverse Polish Notation
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> stack = new Stack<>();
+
+        for(String token : tokens){
+            if (token.matches("-?\\d+")){
+                stack.push(Integer.parseInt(token));
+            } else {
+                int val2 = stack.pop();
+                int val1 = stack.pop();
+
+                switch(token) {
+                    case "+":
+                        stack.push(val1 + val2);
+                        break;
+                    case "-":
+                        stack.push(val1 - val2);
+                        break;
+                    case "*":
+                        stack.push(val1 * val2);
+                        break;
+                    case "/":
+                        stack.push(val1 / val2);
+                        break;
+                }
+            }
+        }
+
+        return stack.pop();
+    }
+
 
     //155. Min Stack
     public class MinStack {
