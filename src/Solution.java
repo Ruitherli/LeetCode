e6f67b544840ff771337bdc71d6665e4fb51ca19
Heng Rui Kang <125897596+Ruitherli@users.noreply.github.com>
2023-07-12 18:19:50 +0800

    Solution for 739. Daily Temperatures

--- src/Solution.java
+++ src/Solution.java
@@ -658,6 +658,21 @@ public class Solution {
         return top;
     }
 
+    //739. Daily Temperatures
+    public int[] dailyTemperatures(int[] temperatures) {
+        int n = temperatures.length;
+        int[] res = new int[n];
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < n; i++) {
+            while (!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]) {
+                int idx = stack.pop();
+                res[idx] = i - idx;
+            }
+            stack.push(i);
+        }
+        return res;
+    }
+
     //744. Find Smallest Letter Greater Than Target
     public char nextGreatestLetter(char[] letters, char target) {
         // Initialize result to be the maximum character value
