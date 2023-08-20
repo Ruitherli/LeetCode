3d477c04f084f475f028dcc6ccaeaa92ff7982c0
Heng Rui Kang <125897596+Ruitherli@users.noreply.github.com>
2023-08-20 13:31:35 +0800

    Solution for 153. Find Minimum in Rotated Sorted Array

--- src/Solution.java
+++ src/Solution.java
@@ -541,6 +541,28 @@ public class Solution {
         return stack.pop();
     }
 
+    //153. Find Minimum in Rotated Sorted Array
+    public int findMin(int[] nums) {
+        int res = nums[0];
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right){
+         if (nums[left] < nums[right]){
+             res = Math.min(res,nums[left]);
+             break;
+         }
+         int mid = left + (right - left)/2;
+         res = Math.min(res,nums[mid]);
+
+         if (nums[mid] >= nums[left]){
+             left = mid + 1;
+         } else {
+             right = mid - 1;
+         }
+        }
+        return res;
+    }
 
     //155. Min Stack
     public class MinStack {
