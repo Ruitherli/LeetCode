b9c660ad1436fa81b75dc038d579c5240570f1f9
Heng Rui Kang <125897596+Ruitherli@users.noreply.github.com>
2023-08-28 14:21:15 +0800

    Solution for 225. Implement Stack using Queues //Use two queues

--- src/Solution.java
+++ src/Solution.java
@@ -721,6 +721,51 @@ public class Solution {
         return false;
     }
 
+
+    //225. Implement Stack using Queues
+    class MyStack {
+        private Queue<Integer> queue1;
+        private Queue<Integer> queue2;
+
+        public MyStack() {
+            queue1 = new LinkedList<>();
+            queue2 = new LinkedList<>();
+        }
+
+        public void push(int x) {
+            // Enqueue new element to queue2
+            queue2.add(x);
+
+            // Dequeue all elements from queue1 and enqueue to queue2
+            while (!queue1.isEmpty()) {
+                queue2.add(queue1.remove());
+            }
+
+            // Swap the names of the two queues
+            Queue<Integer> temp = queue1;
+            queue1 = queue2;
+            queue2 = temp;
+        }
+
+        public int pop() {
+            if (queue1.isEmpty()) {
+                throw new RuntimeException("Stack is empty");
+            }
+            return queue1.remove();
+        }
+
+        public int top() {
+            if (queue1.isEmpty()) {
+                throw new RuntimeException("Stack is empty");
+            }
+            return queue1.peek();
+        }
+
+        public boolean empty() {
+            return queue1.isEmpty();
+        }
+    }
+
     //238. Product of Array Except Self
     public int[] productExceptSelf(int[] nums) {
         int size = nums.length;
