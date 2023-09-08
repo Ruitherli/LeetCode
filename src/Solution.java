3fcc42be78e6d555b21341b07096e2527d3f77a9
Heng Rui Kang <125897596+Ruitherli@users.noreply.github.com>
2023-09-08 22:03:56 +0800

    Solution for 543. Diameter of Binary Tree //

--- src/Solution.java
+++ src/Solution.java
@@ -1154,6 +1154,23 @@ public class Solution {
         return max;
     }
 
+    //543. Diameter of Binary Tree
+    int result = -1;
+
+    public int diameterOfBinaryTree(TreeNode root) {
+        dfs(root);
+        return result;
+    }
+
+    private int dfs(TreeNode current) {
+        if (current == null) {
+            return -1;
+        }
+        int left = 1 + dfs(current.left);
+        int right = 1 + dfs(current.right);
+        result = Math.max(result, (left + right));
+        return Math.max(left, right);
+    }
     //567. Permutation in String
     public boolean checkInclusion(String s1, String s2) {
         int sizeOne = s1.length();
