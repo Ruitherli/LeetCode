d1433011bc92890b7d5240526a33dbc2407a7fa5
Heng Rui Kang <125897596+Ruitherli@users.noreply.github.com>
2023-09-11 22:02:33 +0800

    Solution for 70. Climbing Stairs //Fibonacci Sequence

--- src/Solution.java
+++ src/Solution.java
@@ -541,6 +541,23 @@ public class Solution {
         return new ArrayList<>(map.values());
     }
 
+    //70. Climbing Stairs
+    public int climbStairs(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        if (n == 2) {
+            return 2;
+        }
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+
     //74. Search a 2D Matrix
     public boolean searchMatrix(int[][] matrix, int target) {
         int m = matrix.length;
