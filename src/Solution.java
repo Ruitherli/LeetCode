98244c2e760c02d77f9bf9bb53967247d0911ffa
Heng Rui Kang <125897596+Ruitherli@users.noreply.github.com>
2023-09-17 21:59:33 +0800

    Solution for 58. Length of Last Word //Start from backwards, if a char is found, start counting until a white space is found

--- src/Solution.java
+++ src/Solution.java
@@ -541,6 +541,22 @@ public class Solution {
         return new ArrayList<>(map.values());
     }
 
+    //58. Length of Last Word
+    public int lengthOfLastWord(String s) {
+        int length = 0;
+
+        // We are looking for the last word so let's go backward
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) != ' ') { // a letter is found so count
+                length++;
+            } else {  // it's a white space instead
+                //  Did we already started to count a word ? Yes so we found the last word
+                if (length > 0) return length;
+            }
+        }
+        return length;
+    }
+
     //70. Climbing Stairs
     public int climbStairs(int n) {
         if (n == 1) {
