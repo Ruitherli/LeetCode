c33277b5d4a8a3a370f25ad3577d1a1b5e947788
Heng Rui Kang <125897596+Ruitherli@users.noreply.github.com>
2023-09-23 16:27:43 +0800

    Solution for 338. Counting Bits // For every even number `i`, the number of 1 bits will be the same as `i/2`. For every odd number `i`, the number of 1 bits will be the same as `i/2 + 1`

--- src/Solution.java
+++ src/Solution.java
@@ -1270,6 +1270,21 @@ public class Solution {
             }
         }
 
+    //338. Counting Bits
+    public int[] countBits(int n) {
+        int[] ans = new int[n+1];
+        ans[0] = 0; // base case
+
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 == 0) {
+                ans[i] = ans[i/2];
+            } else {
+                ans[i] = ans[i/2] + 1;
+            }
+        }
+        return ans;
+    }
+
 
     //347. Top K Frequent Elements
     public int[] topKFrequent(int[] nums, int k) {
