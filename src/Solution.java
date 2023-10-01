8de7138aff530d7d0f6fab9ded99ff1e0b57ee98
Heng Rui Kang <125897596+Ruitherli@users.noreply.github.com>
2023-10-01 22:36:49 +0800

    Solution for 500. Keyboard Row

--- src/Solution.java
+++ src/Solution.java
@@ -1510,6 +1510,28 @@ public class Solution {
         return count;
     }
 
+    //500. Keyboard Row
+    public String[] findWords(String[] words) {
+        int[] rows = {2,3,3,2,1,2,2,2,1,2,2,2,3,3,1,1,1,1,2,1,1,3,1,3,1,3};
+        List<String> list = new ArrayList<>();
+        for(String a: words){
+            String s = a.toLowerCase();
+            boolean isValid = true;
+            int row = rows[s.charAt(0)-'a'];
+            for(int i=1; i<s.length();i++){
+                if(rows[s.charAt(i)-'a']!=row){
+                    isValid = false;
+                    break;
+                }
+            }
+            if(isValid){
+                list.add(a);
+            }
+
+        }
+        return list.toArray(new String[0]);
+    }
+
     //543. Diameter of Binary Tree
     int result = -1;
 
